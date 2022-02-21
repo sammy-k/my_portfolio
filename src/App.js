fb9729685e44ddf79beb1ad511f6fef57a2b25b7
Sam Kanan <samkanan@gmail.com>
2022-02-21 16:34:17 -0600

    added skills component

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
       <Navbar />
       <About />
       <Projects />
-      {/* <Skills /> */}
+      <Skills />
       {/* <Contact /> */}
       {/* <Footer /> */}
     </div>
